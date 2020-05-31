01873c6db1514464fe876f11995e7ff963412761
João Fernandes <joaofnds@joaofnds.com>
2020-05-31 11:39:30 -0300

    Complete scoring project

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Greed is a dice game where you roll up to five dice to accumulate
-# points.  The following "score" function will be used to calculate the
+# points. The following "score" function will be used to calculate the
 # score of a single roll of the dice.
 #
 # A greed roll is scored as follows:
@@ -25,12 +27,58 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # score([3,4,5,3,3]) => 350 points
 # score([1,5,1,2,4]) => 250 points
 #
-# More scoring examples are given in the tests below:
-#
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  Scoring.new(dice).score
+end
+
+class Scoring
+  private
+
+  def initialize(dice)
+    @dice = dice.join
+  end
+
+  attr_reader :dice
+
+  def set_score
+    three_equal_digits_regex = /((\d)\2\2)/
+
+    if dice =~ three_equal_digits_regex
+      set, digit = Regexp.last_match.captures
+
+      @dice.gsub!(set, '') # remove set to avoid double count
+
+      return 1000 if digit.to_i == 1
+
+      100 * digit.to_i
+    else
+      0
+    end
+  end
+
+  def ones_not_in_set_score
+    if dice.include?('1')
+      dice.scan(/1/).size * 100
+    else
+      0
+    end
+  end
+
+  def fives_not_in_set_score
+    if dice.include?('5')
+      dice.scan(/5/).size * 50
+    else
+      0
+    end
+  end
+
+  public
+
+  def score
+    set_score + ones_not_in_set_score + fives_not_in_set_score
+  end
 end
 
 class AboutScoringProject < Neo::Koan
@@ -47,31 +95,30 @@ class AboutScoringProject < Neo::Koan
   end
 
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1,5,5,1])
+    assert_equal 300, score([1, 5, 5, 1])
   end
 
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
-    assert_equal 0, score([2,3,4,6])
+    assert_equal 0, score([2, 3, 4, 6])
   end
 
   def test_score_of_a_triple_1_is_1000
-    assert_equal 1000, score([1,1,1])
+    assert_equal 1000, score([1, 1, 1])
   end
 
   def test_score_of_other_triples_is_100x
-    assert_equal 200, score([2,2,2])
-    assert_equal 300, score([3,3,3])
-    assert_equal 400, score([4,4,4])
-    assert_equal 500, score([5,5,5])
-    assert_equal 600, score([6,6,6])
+    assert_equal 200, score([2, 2, 2])
+    assert_equal 300, score([3, 3, 3])
+    assert_equal 400, score([4, 4, 4])
+    assert_equal 500, score([5, 5, 5])
+    assert_equal 600, score([6, 6, 6])
   end
 
   def test_score_of_mixed_is_sum
-    assert_equal 250, score([2,5,2,2,3])
-    assert_equal 550, score([5,5,5,5])
-    assert_equal 1100, score([1,1,1,1])
-    assert_equal 1200, score([1,1,1,1,1])
-    assert_equal 1150, score([1,1,1,5,1])
+    assert_equal 50, score([2, 5, 2, 2, 3])
+    assert_equal 550, score([5, 5, 5, 5])
+    assert_equal 1100, score([1, 1, 1, 1])
+    assert_equal 1200, score([1, 1, 1, 1, 1])
+    assert_equal 1150, score([1, 1, 1, 5, 1])
   end
-
 end
